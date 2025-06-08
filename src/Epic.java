cc6943b50a2717b5dc2b040e2e7a12148d7fbcd4
R0flyan <max.sitosenko@gmail.com>
2025-06-08 10:24:36 +0300

    add task classes

---
+++ src/Epic.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private List<Integer> subtaskIds;
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, TaskStatus.NEW);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,56 @@
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task(int id, String name, String description, TaskStatus status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
